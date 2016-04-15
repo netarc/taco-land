1c4e6df45ee0d4007d2d4e55c0882926f387c886
Josh Hollenbeck <josh.hollenbeck@citrusbyte.com>
2016-04-15 16:13:14 -0700

    Update research vein spawning

--- modules/research/control.lua
+++ modules/research/control.lua
@@ -1,27 +1,40 @@
+require "util"
 require "defines"
+
 local events = require("util.events")
 local config = require("config")
 
-local ResearchNode = "quantum-vein"
+local cfg_entity_name = "quantum-vein"
 
-function SpawnCrystalVein(AREA, MAX_AMOUNT, CHUNKS, STATEMENT, event)
-	local PosX = event.area.left_top.x+math.random(AREA)
-	local PosY = event.area.left_top.y+math.random(AREA)
+
+function spawn_quantum_vein(event, area)
+	local PosX = event.area.left_top.x + math.random(area)
+	local PosY = event.area.left_top.y + math.random(area)
 	local surface = game.get_surface("nauvis")
 
-	if surface.can_place_entity{name=(ResearchNode), position={PosX,PosY}} then
-		surface.create_entity({
-      name = ResearchNode,
+	if surface.can_place_entity{name=cfg_entity_name, position={PosX,PosY}} then
+		local entity = surface.create_entity({
+      name = cfg_entity_name,
       position = {PosX, PosY},
       force = game.forces.neutral
     })
+
+		table.insert(global.tacoland.research.veins, entity)
 	end
 end
 
 -- Chunks are 32x32
 events.on_event(defines.events.on_chunk_generated, function(event)
-	-- TODO: we only want to spawn a quantum vein spaced out great distance from each other
-	SpawnCrystalVein(63, 10, 1000, false, event)
+	if math.random(100) <= 10 then
+		-- abort if we are within X distance of another vein
+		for _,vein in pairs(global.tacoland.research.veins) do
+			if util.distance(event.area.left_top, vein.position) < 3500 then
+				return
+			end
+		end
+
+		spawn_quantum_vein(event, 32)
+	end
 end)
 
 events.on_event(defines.events.on_tick, function(event)
---
+++ modules/research/globals.lua
@@ -0,0 +1,7 @@
+return {
+  tacoland = {
+    research = {
+      veins = {}
+    }
+  }
+}
