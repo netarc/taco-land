7ff86253613cc816c85d04927da702b04358d463
Josh Hollenbeck <josh.hollenbeck@citrusbyte.com>
2016-04-16 18:25:06 -0700

    Disable world-loot

--- modules/resource-expansion/control.lua
+++ modules/resource-expansion/control.lua
@@ -4,7 +4,7 @@ local events = require("util.events")
 
 -- TODO: we really need to make this smarter by tracking existing fields and comparing
 -- distance to similiar field types instead of just distance from start pos (0,0)
-events.on_event(defines.events.on_chunk_generated, function(event)
+events.on(defines.events.on_chunk_generated, function(event)
 	local distX = math.abs(event.area.left_top.x)
 	local distY = math.abs(event.area.left_top.y)
 	local distance = math.floor(math.sqrt((distX * distX) + (distY * distY)) / 32)
