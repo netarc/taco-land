baff0c2a2bea866563c3be2ed4b74e37c56382c5
Josh Hollenbeck <josh.hollenbeck@citrusbyte.com>
2016-04-17 17:40:12 -0700

    Cleanups to world-loot

--- modules/world-loot/control.lua
+++ modules/world-loot/control.lua
@@ -5,24 +5,23 @@ local logger = require("util.logger")
 
 local function get_random_cache()
 	local roll = math.random(100)
-	
-	-- 5% legendary (1,2,3 on the roll)
+
+	-- 5% legendary
 	if (roll <= 5) then
 		return global.tacoland.world_loot.caches["legendary"]
-	-- 10% epic (5-15 on the roll)
-	elseif (roll <= 15 and roll > 5) then
+	-- 10% epic
+	elseif (roll <= 15) then
 		return global.tacoland.world_loot.caches["epic"]
-	-- 25% rare (15-40 on the roll)
-	elseif (roll <= 40 and roll > 15) then
+	-- 25% rare
+	elseif (roll <= 40) then
 		return global.tacoland.world_loot.caches["rare"]
-	-- 60% basic (40-100 on the roll )
-	elseif ( roll > 40) then
+	-- 60% basic
+	else
 		return global.tacoland.world_loot.caches["basic"]
 	end
 end
 
 function spawn_cache(event, area)
-
 	local cache = get_random_cache()
 
 	if not cache or #cache.loot == 0 then
@@ -35,7 +34,11 @@ function spawn_cache(event, area)
 	}
 
 	if game.get_surface("nauvis").can_place_entity({name=cache.container, position=pos}) then
-		local container = game.get_surface("nauvis").create_entity({name=cache.container, position=pos, force=game.forces.neutral})
+		local container = game.get_surface("nauvis").create_entity({
+			name=cache.container,
+			position=pos,
+			force=game.forces.neutral
+		})
 
 		insert_loot(container, cache.loot, 5, 5)
 		insert_loot(container, global.tacoland.world_loot.raw_resources, 3, 100)
@@ -43,7 +46,6 @@ function spawn_cache(event, area)
 		if (cache.container ~= "basic-cache") then
 			insert_loot(container, global.tacoland.world_loot.advanced_materials, 3, 20)
 		end
-    
 	end
 end
 
@@ -56,19 +58,11 @@ function insert_loot(container, loot_table, num_items, max_stack_size)
 	end
 end
 
-events.on_init(function()
-
-end)
-
-events.on_load(function()
-
-end)
-
 -- Chunks are 32x32
 events.on(defines.events.on_chunk_generated, function(event)
 	local roll = math.random(100)
 	-- 10% to spawn a cache
-	if (roll <= 10 ) then
+	if (roll <= 10) then
 		spawn_cache(event, 32)
 	end
 end)
